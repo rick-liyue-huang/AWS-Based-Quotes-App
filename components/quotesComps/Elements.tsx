915c5b9e22aae4d2ea03cc1907ebcf9f057ceceb
Rick Huang <rick.liyue.huang@gmail.com>
2023-11-17 15:13:34 +1100

    feat: add background on page

--- components/quotesComps/Elements.tsx
+++ components/quotesComps/Elements.tsx
@@ -1,5 +1,55 @@
-import styled from 'styled-components';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+import styled from 'styled-components';
 
-export const BackgroundComponent = styled.div``;
+export const BackgroundComponent = styled.div`
+  position: fixed;
+  background: linear-gradient(to right, #0b7dae, #c28d);
+  background-size: 200% 200%;
+  animation: gradient 8s ease infinite;
+  height: 100vh;
+  width: 100vw;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  opacity: 0.5;
+  z-index: 2;
+  margin-left: 0px;
+  margin-top: 0px;
+  scale: 0.8;
+`;
+
+export const FooterComponent = styled.footer`
+  width: 100vw;
+  height: 10vh;
+  text-align: center;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  position: absolute;
+  bottom: 0;
+  color: #fff;
+  z-index: 10;
+`;
+
+export const FooterLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  font-weight: bold;
+  &:hover {
+    color: #fff;
+    text-decoration: none;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
+import Clouds1 from '@/assets/background.png';
+import {
+  BackgroundComponent,
+  BackgroundImage1,
+  FooterComponent,
+  FooterLink,
+} from '@/components/quotesComps/Elements';
 import Head from 'next/head';
+import React from 'react';
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = React.useState<number | null>(0);
+
   return (
     <>
       <Head>
@@ -9,6 +19,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <BackgroundComponent>
+        <BackgroundImage1 src={Clouds1} sizes="contain" alt="background" />
+        <FooterComponent>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with{' '}
+            <FooterLink
+              href="https://github.com/rick-liyue-huang/AWS-Based-Quotes-App"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @RickHuang
+            </FooterLink>
+          </>
+        </FooterComponent>
+      </BackgroundComponent>
     </>
   );
 }
