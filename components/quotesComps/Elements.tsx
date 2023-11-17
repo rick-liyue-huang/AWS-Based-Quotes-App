8dcef9b23f937a098e42d9716c45e6f95b3676e1
Rick Huang <rick.liyue.huang@gmail.com>
2023-11-17 20:06:56 +1100

    feat: add mui modal on page

--- components/quotesComps/Elements.tsx
+++ components/quotesComps/Elements.tsx
@@ -53,3 +53,95 @@ export const FooterLink = styled(Link)`
     text-decoration: none;
   }
 `;
+
+export const QuoteGeneratorContainer = styled.div`
+  min-height: 350px;
+  min-width: 350px;
+  height: 70vh;
+  width: 70vw;
+  border: 2px solid口#ffffff22;
+  border-radius: 15px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  z-index: 20;
+  background: rgba(0, 0, 70, 0.3);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+
+export const QuoteGeneratorComponent = styled.div`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  width: 100%;
+`;
+
+export const QuoteGeneratorTitle = styled.h1`
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 50px;
+  text-align: center;
+  color: #fff;
+  padding: 0px 20px 0 20px;
+  position: relative;
+  /* for mobile */
+  @media only screen and (max-width: 600px) {
+    font-size: 30px;
+  }
+`;
+
+export const QuoteGeneratorDescription = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 35px;
+  text-align: center;
+  color: #fff;
+  padding: 0px 20px 0 20px;
+  position: relative;
+  /* for mobile */
+  @media only screen and (max-width: 600px) {
+    font-size: 15px;
+  }
+`;
+
+export const QuoteGeneratorButton = styled.div`
+  height: 100px;
+  width: 300px;
+  border: 2px solid grey;
+  border-radius: 15px;
+  margin-top: 20px;
+  position: relative;
+  transition: 0.2s all ease-in-out;
+  cursor: pointer;
+  top: 20px;
+  margin: auto;
+  transform-origin: center;
+  background: rgba(0, 0, 70, 0.3);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  &:hover {
+    filter: brightness(3);
+    transition: 0.2s all ease-in-out;
+    transform: scale(1.1);
+    transform-origin: center;
+  }
+`;
+
+export const ButtonText = styled.p`
+  color: #fff;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 35px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  width: 100%;
+  text-align: center;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import Clouds1 from '@/assets/background.png';
 import {
   BackgroundComponent,
   BackgroundImage1,
+  ButtonText,
   FooterComponent,
   FooterLink,
+  QuoteGeneratorButton,
+  QuoteGeneratorComponent,
+  QuoteGeneratorContainer,
+  QuoteGeneratorDescription,
+  QuoteGeneratorTitle,
 } from '@/components/quotesComps/Elements';
 import Head from 'next/head';
 import React from 'react';
@@ -20,6 +26,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <BackgroundComponent>
+        {/* Quote Modal */}
+        {/* <QuoteGeneratorModal /> */}
+
+        <QuoteGeneratorContainer>
+          <QuoteGeneratorComponent>
+            <QuoteGeneratorTitle>Quote Generator</QuoteGeneratorTitle>
+            <QuoteGeneratorDescription>
+              Generate a random quote from the database. You can also add your
+              own quote from{' '}
+              <FooterLink
+                href="https://zenquotes.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </FooterLink>
+              .
+            </QuoteGeneratorDescription>
+            <QuoteGeneratorButton>
+              <ButtonText onClick={null}>Pop one Quote</ButtonText>
+            </QuoteGeneratorButton>
+          </QuoteGeneratorComponent>
+        </QuoteGeneratorContainer>
+
         <BackgroundImage1 src={Clouds1} sizes="contain" alt="background" />
         <FooterComponent>
           <>
